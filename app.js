8e98a043ce6118295436667a6e66cb5c889a7bf2
timmysutanto <43670659+timmysutanto@users.noreply.github.com>
2019-05-19 17:51:18 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const app = express();
 const cypherRoutes = require('./API/cypher');
 const decypherRoutes = require('./API/decypher');
 const vigenereRoutes = require('./API/vigenere');
+const devigenereRoutes = require('./API/devigenere');
 
 app.use('/cypher', cypherRoutes);
 app.use('/decypher', decypherRoutes);
 app.use('/vigenere', vigenereRoutes);
+app.use('/devigenere', devigenereRoutes);
 
 module.exports = app;
